14205314b13d8efde7f19868ffccf91cbe9b1a60
Matthew Sykes <sykesmat@us.ibm.com>
2020-07-13 17:33:44 -0400

    Add some documentation to test helpers
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- pkg/tested/tested.go
+++ pkg/tested/tested.go
@@ -18,6 +18,18 @@ type tHelper interface {
 	Helper()
 }
 
+// TempDir creates a temporary directory and returns the path to the directory
+// and a function that should be called to cleanup. Any errors encountered
+// while creating or removing the temporary directory will result in a test
+// failure.
+//
+// The general pattern looks like this:
+//
+//     temp, cleanup := TempDir(t, "")
+//     defer cleanup()
+//
+// When TB.TestDir and TB.Cleanup arrive in Go 1.15, this will no longer be
+// necessary.
 func TempDir(t TestingT, dir, pattern string) (tempdir string, cleanup func()) {
 	if h, ok := t.(tHelper); ok {
 		h.Helper()
@@ -29,18 +41,19 @@ func TempDir(t TestingT, dir, pattern string) (tempdir string, cleanup func()) {
 	}
 	cleanup = func() {
 		if err := os.RemoveAll(td); err != nil {
-			t.Errorf("remove tempdir: %v", err)
+			t.Errorf("failed to remove tempdir: %v", err)
 		}
 	}
 	return td, cleanup
 }
 
+// Close closes an io.Closer and will fail the test if Close fails.
 func Close(t TestingT, c io.Closer) {
 	if h, ok := t.(tHelper); ok {
 		h.Helper()
 	}
 
 	if err := c.Close(); err != nil {
-		t.Errorf("close: %v", err)
+		t.Errorf("failed to close: %v", err)
 	}
 }
