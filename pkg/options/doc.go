d4e56ab1fd8262905a01dac9afee79cd12b42779
Matthew Sykes <sykesmat@us.ibm.com>
2020-09-01 12:23:17 -0400

    Experiment with Flags and configuration options
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

---
+++ pkg/options/doc.go
@@ -0,0 +1,10 @@
+// Copyright IBM Corp. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// Options is a placeholder for configuration structures and command line
+// flags.  It's likely that the various options will be moved out of this
+// package to live closer to the items that are configured.
+//
+// More succinctly, this is an experiment to see how component configuration
+// may work. If it doesn't, we want to throw it away quickly.
+package options
