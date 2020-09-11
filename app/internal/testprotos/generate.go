844012413620a4bbb7b8a436759985f886eb9e89
Matthew Sykes <sykesmat@us.ibm.com>
2020-09-11 16:02:07 -0400

    Remove app/internal as code is no longer needed
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- app/internal/testprotos/generate.go
+++
@@ -1,6 +0,0 @@
-// Copyright IBM Corp. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-package testprotos
-
-//go:generate protoc -I . --go_out=plugins=grpc,paths=source_relative:. ./test.proto
