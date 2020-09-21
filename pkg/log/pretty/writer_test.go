31b289d1f7dc9462b08b6fd46a1534d3776c930e
Danny Cao <dcao@us.ibm.com>
2020-09-21 17:24:51 -0400

    Fix pretty writer test for different time zone (CI)
    Signed-off-by: Danny Cao <dcao@us.ibm.com>

--- pkg/log/pretty/writer_test.go
+++ pkg/log/pretty/writer_test.go
@@ -23,10 +23,9 @@ func TestWrite(t *testing.T) {
 	gt.Expect(err).To(MatchError("not a logfmt string"))
 
 	testLine = `ts=1600356328.141956 level=info logger=batik caller=app/start.go:54 msg="Starting server"`
-	expectedLine := `"\x1b[37mSep 17 11:25:28.000000\x1b[0m |\x1b[36mINFO\x1b[0m| \x1b[34mbatik\x1b[0m \x1b[0mapp/start.go:54\x1b[0m \x1b[97mStarting server\x1b[0m \n"`
+	expectedLine := `"\\x1b\[37mSep 17 [0-9:]*\.000000\\x1b\[0m \|\\x1b\[36mINFO\\x1b\[0m\| \\x1b\[34mbatik\\x1b\[0m \\x1b\[0mapp/start.go:54\\x1b\[0m \\x1b\[97mStarting server\\x1b\[0m \\n"`
 	n, err := w.Write([]byte(testLine))
 	gt.Expect(err).NotTo(HaveOccurred())
 	gt.Expect(n).To(Equal(113))
-	fmt.Println(buf.String())
-	gt.Expect(fmt.Sprintf("%q", buf.String())).To(Equal(expectedLine))
+	gt.Expect(fmt.Sprintf("%q", buf.String())).To(MatchRegexp(expectedLine))
 }
