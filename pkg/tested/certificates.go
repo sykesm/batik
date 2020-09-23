191d2ca8670a7b7eb7840049673c21170d05a09c
Matthew Sykes <matthew.sykes@gmail.com>
2020-09-23 12:20:51 -0400

    Add TLS certificate helpers to tested
    Signed-off-by: Matthew Sykes <matthew.sykes@gmail.com>

---
+++ pkg/tested/certificates.go
@@ -0,0 +1,180 @@
+// Copyright IBM Corp. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package tested
+
+import (
+	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"net"
+	"time"
+)
+
+type CertKeyPair struct {
+	Cert        []byte
+	Key         []byte
+	Certificate tls.Certificate
+}
+
+type CA CertKeyPair
+
+func NewCA(t TestingT, subjectCN string) CA {
+	cert, key := generateCA(t, subjectCN)
+	certificate, err := tls.X509KeyPair(cert, key)
+	assertNoError(t, err)
+	return CA{Cert: cert, Key: key, Certificate: certificate}
+}
+
+func (ca CA) IssueServerCertificate(t TestingT, subjectCN string, sans ...string) CertKeyPair {
+	cacert, err := x509.ParseCertificate(ca.Certificate.Certificate[0])
+	assertNoError(t, err)
+
+	privateKey, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	assertNoError(t, err)
+	publicKey := privateKey.Public()
+
+	template := newTemplate(t, subjectCN, sans...)
+	template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth}
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, cacert, publicKey, ca.Certificate.PrivateKey)
+	assertNoError(t, err)
+
+	cert, key := pemEncode(t, derBytes, privateKey)
+	certificate, err := tls.X509KeyPair(join(cert, ca.Cert), key)
+	assertNoError(t, err)
+
+	return CertKeyPair{Cert: cert, Key: key, Certificate: certificate}
+}
+
+func (ca CA) IssueClientCertificate(t TestingT, subjectCN string, sans ...string) CertKeyPair {
+	cacert, err := x509.ParseCertificate(ca.Certificate.Certificate[0])
+	assertNoError(t, err)
+
+	privateKey, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	assertNoError(t, err)
+	publicKey := privateKey.Public()
+
+	template := newTemplate(t, subjectCN, sans...)
+	template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth}
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, cacert, publicKey, ca.Certificate.PrivateKey)
+	assertNoError(t, err)
+
+	cert, key := pemEncode(t, derBytes, privateKey)
+	certificate, err := tls.X509KeyPair(join(cert, ca.Cert), key)
+	assertNoError(t, err)
+
+	return CertKeyPair{Cert: cert, Key: key, Certificate: certificate}
+}
+
+func (ckp CertKeyPair) ServerTLSConfig(t TestingT, clientCA *tls.Certificate) *tls.Config {
+	caCertPool := x509.NewCertPool()
+	for i := 1; i < len(ckp.Certificate.Certificate); i++ {
+		caCertPool.AppendCertsFromPEM(ckp.Certificate.Certificate[i])
+	}
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{ckp.Certificate},
+		RootCAs:      caCertPool,
+	}
+	if clientCA != nil {
+		tlsConfig.ClientCAs = x509.NewCertPool()
+		cacert, err := x509.ParseCertificate(clientCA.Certificate[0])
+		assertNoError(t, err)
+		tlsConfig.ClientCAs.AddCert(cacert)
+		tlsConfig.ClientAuth = tls.VerifyClientCertIfGiven
+	}
+	return tlsConfig
+}
+
+func (ckp CertKeyPair) ClientTLSConfig(t TestingT, serverCA *tls.Certificate) *tls.Config {
+	caCertPool := x509.NewCertPool()
+	cacert, err := x509.ParseCertificate(serverCA.Certificate[0])
+	assertNoError(t, err)
+	caCertPool.AddCert(cacert)
+
+	return &tls.Config{
+		Certificates:       []tls.Certificate{ckp.Certificate},
+		RootCAs:            caCertPool,
+		ClientSessionCache: tls.NewLRUClientSessionCache(10),
+	}
+}
+
+func generateCA(t TestingT, subjectCN string) (pemCert, pemKey []byte) {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	assertNoError(t, err)
+	publicKey := privateKey.Public()
+
+	template := newTemplate(t, subjectCN)
+	template.KeyUsage |= x509.KeyUsageCertSign
+	template.IsCA = true
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, publicKey, privateKey)
+	assertNoError(t, err)
+
+	return pemEncode(t, derBytes, privateKey)
+}
+
+func newTemplate(t TestingT, subjectCN string, sans ...string) x509.Certificate {
+	notBefore := time.Now().Add(-1 * time.Minute)
+	notAfter := time.Now().Add(time.Duration(365 * 24 * time.Hour))
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	assertNoError(t, err)
+
+	template := x509.Certificate{
+		Subject:               pkix.Name{CommonName: subjectCN},
+		SerialNumber:          serialNumber,
+		NotBefore:             notBefore,
+		NotAfter:              notAfter,
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		BasicConstraintsValid: true,
+	}
+	for _, s := range sans {
+		if ip := net.ParseIP(s); ip != nil {
+			template.IPAddresses = append(template.IPAddresses, ip)
+		} else {
+			template.DNSNames = append(template.DNSNames, s)
+		}
+	}
+
+	return template
+}
+
+func pemEncode(t TestingT, derCert []byte, key *ecdsa.PrivateKey) (pemCert, pemKey []byte) {
+	certBuf := &bytes.Buffer{}
+	err := pem.Encode(certBuf, &pem.Block{Type: "CERTIFICATE", Bytes: derCert})
+	assertNoError(t, err)
+
+	keyBytes, err := x509.MarshalECPrivateKey(key)
+	assertNoError(t, err)
+
+	keyBuf := &bytes.Buffer{}
+	err = pem.Encode(keyBuf, &pem.Block{Type: "EC PRIVATE KEY", Bytes: keyBytes})
+	assertNoError(t, err)
+
+	return certBuf.Bytes(), keyBuf.Bytes()
+}
+
+func join(b ...[]byte) []byte {
+	var res []byte
+	for _, b := range b {
+		res = append(res, b...)
+	}
+	return res
+}
+
+func assertNoError(t TestingT, err error) {
+	if err != nil {
+		if h, ok := t.(tHelper); ok {
+			h.Helper()
+		}
+		t.Fatalf("unexpected error: %s", err)
+	}
+}
---
+++ pkg/tested/certificates_test.go
@@ -0,0 +1,100 @@
+// Copyright IBM Corp. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package tested
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"net"
+	"testing"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestCertificates(t *testing.T) {
+	ca := NewCA(t, "my-ca")
+
+	t.Run("CA", func(t *testing.T) {
+		gt := NewGomegaWithT(t)
+		cert, err := x509.ParseCertificate(ca.Certificate.Certificate[0])
+		gt.Expect(err).NotTo(HaveOccurred())
+
+		gt.Expect(cert.IsCA).To(BeTrue())
+		gt.Expect(cert.Subject.CommonName).To(Equal("my-ca"))
+		gt.Expect(cert.Subject.CommonName).To(Equal(cert.Issuer.CommonName))
+		gt.Expect(cert.KeyUsage).To(Equal(x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign))
+		gt.Expect(cert.ExtKeyUsage).To(HaveLen(0))
+	})
+
+	t.Run("Server", func(t *testing.T) {
+		gt := NewGomegaWithT(t)
+		srv := ca.IssueServerCertificate(t, "server", "127.0.0.1", "::1", "localhost")
+		cert, err := x509.ParseCertificate(srv.Certificate.Certificate[0])
+		gt.Expect(err).NotTo(HaveOccurred())
+
+		gt.Expect(cert.IsCA).To(BeFalse())
+		gt.Expect(cert.Subject.CommonName).To(Equal("server"))
+		gt.Expect(cert.Issuer.CommonName).To(Equal("my-ca"))
+		gt.Expect(cert.KeyUsage).To(Equal(x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature))
+		gt.Expect(cert.ExtKeyUsage).To(ConsistOf(x509.ExtKeyUsageServerAuth))
+		gt.Expect(cert.IPAddresses).To(ConsistOf(
+			net.IP([]byte{127, 0, 0, 1}),
+			net.IPv6loopback,
+		))
+	})
+
+	t.Run("Client", func(t *testing.T) {
+		gt := NewGomegaWithT(t)
+		clt := ca.IssueClientCertificate(t, "client", "127.0.0.1")
+		cert, err := x509.ParseCertificate(clt.Certificate.Certificate[0])
+		gt.Expect(err).NotTo(HaveOccurred())
+
+		gt.Expect(cert.IsCA).To(BeFalse())
+		gt.Expect(cert.Subject.CommonName).To(Equal("client"))
+		gt.Expect(cert.Issuer.CommonName).To(Equal("my-ca"))
+		gt.Expect(cert.KeyUsage).To(Equal(x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature))
+		gt.Expect(cert.ExtKeyUsage).To(ConsistOf(x509.ExtKeyUsageClientAuth))
+		gt.Expect(cert.IPAddresses).To(ConsistOf([]net.IP{
+			net.IP([]byte{127, 0, 0, 1}),
+		}))
+	})
+}
+
+func TestCertificatesTLSConfig(t *testing.T) {
+	gt := NewGomegaWithT(t)
+	serverCA := NewCA(t, "server-ca")
+	clientCA := NewCA(t, "client-ca")
+
+	srv := serverCA.IssueServerCertificate(t, "server", "127.0.0.1", "::1", "localhost")
+	clt := clientCA.IssueClientCertificate(t, "client", "127.0.0.1")
+
+	serverTLSConfig := srv.ServerTLSConfig(t, &clientCA.Certificate)
+	clientTLSConfig := clt.ClientTLSConfig(t, &serverCA.Certificate)
+
+	lis, err := tls.Listen("tcp", "127.0.0.1:0", serverTLSConfig)
+	gt.Expect(err).NotTo(HaveOccurred())
+
+	serverResultCh := make(chan error, 1)
+	go func() {
+		defer lis.Close()
+		conn, err := lis.Accept()
+		if err != nil {
+			return
+		}
+		defer conn.Close()
+		_, err = conn.Write([]byte("bye"))
+		serverResultCh <- err
+	}()
+
+	conn, err := tls.Dial("tcp", lis.Addr().String(), clientTLSConfig)
+	gt.Expect(err).NotTo(HaveOccurred())
+	defer conn.Close()
+
+	res, err := ioutil.ReadAll(conn)
+	gt.Expect(err).NotTo(HaveOccurred())
+	gt.Expect(string(res)).To(Equal("bye"))
+
+	gt.Eventually(serverResultCh).Should(Receive(BeNil()))
+}
