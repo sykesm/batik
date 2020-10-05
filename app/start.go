16b9e348b17d35dcb30e24b140edfbd75890e92e
Matthew Sykes <sykesmat@us.ibm.com>
2020-10-05 13:38:48 -0400

    Extract start action implementation as function
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- app/start.go
+++ app/start.go
@@ -34,102 +34,104 @@ func startCommand(config *options.Batik, interactive bool) *cli.Command {
 		Usage:       "start the server",
 		Flags:       config.Server.Flags(),
 		Action: func(ctx *cli.Context) error {
-			logger, err := GetLogger(ctx)
-			if err != nil {
-				return cli.Exit(err, exitServerStartFailed)
-			}
-
-			grpcLogger := logger.Named("grpc")
-			grpcServerOptions := config.Server.GRPC.BuildServerOptions()
-
-			grpcServerOptions = append(grpcServerOptions,
-				grpc.ChainUnaryInterceptor(
-					grpclogging.UnaryServerInterceptor(grpcLogger),
-				),
-				grpc.ChainStreamInterceptor(
-					grpclogging.StreamServerInterceptor(grpcLogger),
-				),
-			)
-
-			tlsConf, err := config.Server.TLS.TLSConfig()
-			if err != nil {
-				return cli.Exit(errors.WithMessage(err, "failed to create server"), exitServerCreateFailed)
-			}
-			if tlsConf != nil {
-				grpcServerOptions = append(grpcServerOptions, grpc.Creds(credentials.NewTLS(tlsConf)))
-			}
-
-			grpcServer := grpccomm.NewServer(
-				grpccomm.ServerConfig{
-					ListenAddress: config.Server.GRPC.ListenAddress,
-					Logger:        grpcLogger,
-				},
-				grpcServerOptions...,
-			)
-
-			logger.Debug("initializing database", zap.String("data_dir", config.Ledger.DataDir))
-			db, err := levelDB(ctx, config.Ledger.DataDir)
-			if err != nil {
-				return cli.Exit(errors.Wrap(err, "failed to create server"), exitServerCreateFailed)
-			}
-
-			encodeService := &transaction.EncodeService{}
-			tb.RegisterEncodeTransactionAPIServer(grpcServer.Server, encodeService)
-
-			storeService := store.NewStoreService(db)
-			sb.RegisterStoreAPIServer(grpcServer.Server, storeService)
-
-			mux := gwruntime.NewServeMux()
-			sb.RegisterStoreAPIHandlerServer(context.Background(), mux, storeService)
-
-			httpServer := config.Server.HTTP.BuildServer(tlsConf)
-			httpServer.Handler = mux
-
-			httpRunner := func(signals <-chan os.Signal, ready chan<- struct{}) error {
-				lis, err := net.Listen("tcp", config.Server.HTTP.ListenAddress)
-				if err != nil {
-					return err
-				}
-
-				errCh := make(chan error, 1)
-				if httpServer.TLSConfig == nil {
-					go func() { errCh <- httpServer.Serve(lis) }()
-				} else {
-					go func() { errCh <- httpServer.ServeTLS(lis, "", "") }()
-				}
-
-				close(ready)
-				select {
-				case <-signals:
-					return nil
-				case err := <-errCh:
-					if errors.Is(err, http.ErrServerClosed) {
-						return nil
-					}
-					return err
-				}
-			}
-
-			// SetServer(ctx, server)
-
-			logger.Info(
-				"Starting server",
-				zap.String("grpc-address", config.Server.GRPC.ListenAddress),
-				zap.String("http-address", config.Server.HTTP.ListenAddress),
-			)
-			grpcProcess := ifrit.Invoke(sigmon.New(grpcServer))
-			httpProcess := ifrit.Invoke(sigmon.New(ifrit.RunFunc(httpRunner)))
-			logger.Info("Server started")
-			if !interactive {
-				select {
-				case err := <-grpcProcess.Wait():
-					return err
-				case err := <-httpProcess.Wait():
-					return err
-				}
-			}
-
-			return nil
+			return startAction(ctx, config, interactive)
 		},
 	}
+}
+
+func startAction(ctx *cli.Context, config *options.Batik, interactive bool) error {
+	logger, err := GetLogger(ctx)
+	if err != nil {
+		return cli.Exit(err, exitServerStartFailed)
+	}
+
+	grpcLogger := logger.Named("grpc")
+	grpcServerOptions := config.Server.GRPC.BuildServerOptions()
+
+	grpcServerOptions = append(grpcServerOptions,
+		grpc.ChainUnaryInterceptor(
+			grpclogging.UnaryServerInterceptor(grpcLogger),
+		),
+		grpc.ChainStreamInterceptor(
+			grpclogging.StreamServerInterceptor(grpcLogger),
+		),
+	)
+
+	tlsConf, err := config.Server.TLS.TLSConfig()
+	if err != nil {
+		return cli.Exit(errors.WithMessage(err, "failed to create server"), exitServerCreateFailed)
+	}
+	if tlsConf != nil {
+		grpcServerOptions = append(grpcServerOptions, grpc.Creds(credentials.NewTLS(tlsConf)))
+	}
+
+	grpcServer := grpccomm.NewServer(
+		grpccomm.ServerConfig{
+			ListenAddress: config.Server.GRPC.ListenAddress,
+			Logger:        grpcLogger,
+		},
+		grpcServerOptions...,
+	)
+
+	logger.Debug("initializing database", zap.String("data_dir", config.Ledger.DataDir))
+	db, err := levelDB(ctx, config.Ledger.DataDir)
+	if err != nil {
+		return cli.Exit(errors.Wrap(err, "failed to create server"), exitServerCreateFailed)
+	}
+
+	encodeService := &transaction.EncodeService{}
+	tb.RegisterEncodeTransactionAPIServer(grpcServer.Server, encodeService)
+
+	storeService := store.NewStoreService(db)
+	sb.RegisterStoreAPIServer(grpcServer.Server, storeService)
+
+	mux := gwruntime.NewServeMux()
+	sb.RegisterStoreAPIHandlerServer(context.Background(), mux, storeService)
+
+	httpServer := config.Server.HTTP.BuildServer(tlsConf)
+	httpServer.Handler = mux
+
+	httpRunner := func(signals <-chan os.Signal, ready chan<- struct{}) error {
+		lis, err := net.Listen("tcp", config.Server.HTTP.ListenAddress)
+		if err != nil {
+			return err
+		}
+
+		errCh := make(chan error, 1)
+		if httpServer.TLSConfig == nil {
+			go func() { errCh <- httpServer.Serve(lis) }()
+		} else {
+			go func() { errCh <- httpServer.ServeTLS(lis, "", "") }()
+		}
+
+		close(ready)
+		select {
+		case <-signals:
+			return nil
+		case err := <-errCh:
+			if errors.Is(err, http.ErrServerClosed) {
+				return nil
+			}
+			return err
+		}
+	}
+
+	logger.Info(
+		"Starting server",
+		zap.String("grpc-address", config.Server.GRPC.ListenAddress),
+		zap.String("http-address", config.Server.HTTP.ListenAddress),
+	)
+	grpcProcess := ifrit.Invoke(sigmon.New(grpcServer))
+	httpProcess := ifrit.Invoke(sigmon.New(ifrit.RunFunc(httpRunner)))
+	logger.Info("Server started")
+	if !interactive {
+		select {
+		case err := <-grpcProcess.Wait():
+			return err
+		case err := <-httpProcess.Wait():
+			return err
+		}
+	}
+
+	return nil
 }
