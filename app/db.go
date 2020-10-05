5cc89f518ddfa66db4fe745d19ac143229f3dc3b
Danny Cao <dcao@us.ibm.com>
2020-10-05 15:03:13 -0400

    Extract db subcommands into functions
    Signed-off-by: Danny Cao <dcao@us.ibm.com>

--- app/db.go
+++ app/db.go
@@ -18,102 +18,114 @@ func dbCommand(config *options.Batik) *cli.Command {
 		Name:  "db",
 		Usage: "perform operations against a kv store",
 		Subcommands: []*cli.Command{
-			{
-				Name:  "get",
-				Usage: "get a value from the db",
-				Action: func(ctx *cli.Context) error {
-					db, err := levelDB(ctx, config.Ledger.DataDir)
-					if err != nil {
-						fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
-						return nil
-					}
-
-					key, err := hex.DecodeString(ctx.Args().First())
-					if err != nil {
-						fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
-						return nil
-					}
-
-					val, err := db.Get(key)
-					if err != nil {
-						fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
-						return nil
-					}
-
-					fmt.Fprintf(ctx.App.ErrWriter, "%s\n", hex.Dump(val))
-					return nil
-				},
-			},
-			{
-				Name:  "put",
-				Usage: "store a value in the db",
-				Action: func(ctx *cli.Context) error {
-					db, err := levelDB(ctx, config.Ledger.DataDir)
-					if err != nil {
-						fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
-						return nil
-					}
-
-					key, err := hex.DecodeString(ctx.Args().Get(0))
-					if err != nil {
-						fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
-						return nil
-					}
-					val, err := hex.DecodeString(ctx.Args().Get(1))
-					if err != nil {
-						fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
-						return nil
-					}
-
-					if err := db.Put(key, val); err != nil {
-						fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
-					}
-
-					return nil
-				},
-			},
-			{
-				Name:  "keys",
-				Usage: "dump all keys in the db",
-				Flags: []cli.Flag{
-					&cli.StringFlag{
-						Name:        "prefix",
-						Usage:       "prefix to range over",
-						DefaultText: "",
-					},
-				},
-				Action: func(ctx *cli.Context) error {
-					db, err := levelDB(ctx, config.Ledger.DataDir)
-					if err != nil {
-						fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
-						return nil
-					}
-
-					prefix, err := hex.DecodeString(ctx.String("prefix"))
-					if err != nil {
-						fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
-						return nil
-					}
-
-					iter := db.NewIterator(prefix, nil)
-					keys, err := iter.Keys()
-					if err != nil {
-						fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
-						return nil
-					}
-
-					for _, k := range keys {
-						fmt.Fprintf(ctx.App.ErrWriter, "%s\n", hex.EncodeToString(k))
-					}
-					return nil
-				},
-			},
+			getSubcommand(config.Ledger.DataDir),
+			keysSubcommand(config.Ledger.DataDir),
+			putSubcommand(config.Ledger.DataDir),
 		},
 	}
 
 	sort.Sort(cli.CommandsByName(command.Subcommands))
 
 	return command
+}
+
+func getSubcommand(dataDir string) *cli.Command {
+	return &cli.Command{
+		Name:  "get",
+		Usage: "get a value from the db",
+		Action: func(ctx *cli.Context) error {
+			db, err := levelDB(ctx, dataDir)
+			if err != nil {
+				fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
+				return nil
+			}
+
+			key, err := hex.DecodeString(ctx.Args().First())
+			if err != nil {
+				fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
+				return nil
+			}
+
+			val, err := db.Get(key)
+			if err != nil {
+				fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
+				return nil
+			}
+
+			fmt.Fprintf(ctx.App.ErrWriter, "%s\n", hex.Dump(val))
+			return nil
+		},
+	}
+}
+
+func keysSubcommand(dataDir string) *cli.Command {
+	return &cli.Command{
+		Name:  "keys",
+		Usage: "dump all keys in the db",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "prefix",
+				Usage:       "prefix to range over",
+				DefaultText: "",
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			db, err := levelDB(ctx, dataDir)
+			if err != nil {
+				fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
+				return nil
+			}
+
+			prefix, err := hex.DecodeString(ctx.String("prefix"))
+			if err != nil {
+				fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
+				return nil
+			}
+
+			iter := db.NewIterator(prefix, nil)
+			keys, err := iter.Keys()
+			if err != nil {
+				fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
+				return nil
+			}
+
+			for _, k := range keys {
+				fmt.Fprintf(ctx.App.ErrWriter, "%s\n", hex.EncodeToString(k))
+			}
+			return nil
+		},
+	}
+}
+
+func putSubcommand(dataDir string) *cli.Command {
+	return &cli.Command{
+		Name:  "put",
+		Usage: "store a value in the db",
+		Action: func(ctx *cli.Context) error {
+			db, err := levelDB(ctx, dataDir)
+			if err != nil {
+				fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
+				return nil
+			}
+
+			key, err := hex.DecodeString(ctx.Args().Get(0))
+			if err != nil {
+				fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
+				return nil
+			}
+			val, err := hex.DecodeString(ctx.Args().Get(1))
+			if err != nil {
+				fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
+				return nil
+			}
+
+			if err := db.Put(key, val); err != nil {
+				fmt.Fprintf(ctx.App.ErrWriter, "%s\n", err)
+			}
+
+			return nil
+		},
+	}
 }
 
 func levelDB(ctx *cli.Context, dir string) (*store.LevelDBKV, error) {
