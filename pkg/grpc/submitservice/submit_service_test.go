1feb22276ae53b96108d9c9861d9a83e60204ebe
Matthew Sykes <sykesmat@us.ibm.com>
2020-10-29 14:15:15 -0400

    Basic test coverage for submit service facade
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- pkg/grpc/submitservice/submit_service_test.go
+++ pkg/grpc/submitservice/submit_service_test.go
@@ -5,13 +5,17 @@ package submitservice
 
 import (
 	"context"
-	"encoding/hex"
+	"crypto"
+	"errors"
 	"testing"
 
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	txv1 "github.com/sykesm/batik/pkg/pb/tx/v1"
+	"github.com/sykesm/batik/pkg/store"
 	. "github.com/sykesm/batik/pkg/tested/matcher"
 	"github.com/sykesm/batik/pkg/transaction"
 )
@@ -23,31 +27,53 @@ func (s submitterFunc) Submit(ctx context.Context, tx *transaction.Transaction) 
 }
 
 func TestSubmit(t *testing.T) {
+	gt := NewGomegaWithT(t)
+
+	tx, err := transaction.New(crypto.SHA256, &txv1.Transaction{
+		Salt: []byte("potassium permanganate (KMnO4) is a salt"),
+		Outputs: []*txv1.State{{
+			Info:  &txv1.StateInfo{Kind: "test-kind"},
+			State: []byte("test-state-1"),
+		}},
+	})
+	gt.Expect(err).NotTo(HaveOccurred())
+
 	tests := map[string]struct {
 		req        *txv1.SubmitRequest
+		submitErr  error
 		resp       *txv1.SubmitResponse
 		errMatcher types.GomegaMatcher
 	}{
-		"nil transaction": {
+		"nil signed transaction": {
 			req:        &txv1.SubmitRequest{},
-			resp:       nil,
-			errMatcher: HaveOccurred(),
+			errMatcher: MatchError(status.Errorf(codes.InvalidArgument, "signed transaction was not provided")),
+		},
+		"nil transaction": {
+			req:        &txv1.SubmitRequest{SignedTransaction: &txv1.SignedTransaction{}},
+			errMatcher: MatchError(status.Errorf(codes.InvalidArgument, "transaction was not provided")),
+		},
+		"invalid transaction": {
+			req:        &txv1.SubmitRequest{SignedTransaction: &txv1.SignedTransaction{Transaction: &txv1.Transaction{}}},
+			errMatcher: MatchError(status.Errorf(codes.InvalidArgument, "transaction salt is missing or less than 32 bytes in length")),
 		},
 		"valid transaction": {
-			req: &txv1.SubmitRequest{
-				SignedTransaction: &txv1.SignedTransaction{
-					Transaction: &txv1.Transaction{
-						Salt: []byte("potassium permanganate (KMnO4) is a salt"),
-						Outputs: []*txv1.State{{
-							Info:  &txv1.StateInfo{Kind: "test-kind"},
-							State: []byte("test-state-1"),
-						}},
-					},
-				},
-			},
-			resp: &txv1.SubmitResponse{
-				Txid: fromHex(t, "5cfb2ad672e2ac73ff7d8d008bf1e8bb32224279722a5ee562f3d3a8726f277e"),
-			},
+			req:  &txv1.SubmitRequest{SignedTransaction: &txv1.SignedTransaction{Transaction: tx.Tx}},
+			resp: &txv1.SubmitResponse{Txid: tx.ID},
+		},
+		"already exists error": {
+			req:        &txv1.SubmitRequest{SignedTransaction: &txv1.SignedTransaction{Transaction: tx.Tx}},
+			submitErr:  &store.AlreadyExistsError{Err: errors.New("already-exists")},
+			errMatcher: MatchError(status.Errorf(codes.AlreadyExists, "storing transaction %s failed: already-exists", tx.ID)),
+		},
+		"not found error": {
+			req:        &txv1.SubmitRequest{SignedTransaction: &txv1.SignedTransaction{Transaction: tx.Tx}},
+			submitErr:  &store.NotFoundError{Err: errors.New("not-found")},
+			errMatcher: MatchError(status.Errorf(codes.FailedPrecondition, "storing transaction %s failed: not-found", tx.ID)),
+		},
+		"unknown error": {
+			req:        &txv1.SubmitRequest{SignedTransaction: &txv1.SignedTransaction{Transaction: tx.Tx}},
+			submitErr:  errors.New("woops"),
+			errMatcher: MatchError(status.Errorf(codes.Unknown, "storing transaction %s failed: woops", tx.ID)),
 		},
 	}
 
@@ -55,7 +81,7 @@ func TestSubmit(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			gt := NewGomegaWithT(t)
 			var submitter submitterFunc = func(ctx context.Context, tx *transaction.Transaction) error {
-				return nil
+				return tt.submitErr
 			}
 			ss := NewSubmitService(submitter)
 			resp, err := ss.Submit(context.Background(), tt.req)
@@ -67,12 +93,4 @@ func TestSubmit(t *testing.T) {
 			gt.Expect(resp).To(ProtoEqual(tt.resp))
 		})
 	}
-}
-
-func fromHex(t *testing.T, s string) []byte {
-	b, err := hex.DecodeString(s)
-	if err != nil {
-		t.Fatalf("failed to decode %q as hex string", s)
-	}
-	return b
 }
