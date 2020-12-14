bea8409d6431202ef443e4623a8cb9823b1a82c2
Matthew Sykes <sykesmat@us.ibm.com>
2020-12-14 15:25:06 -0500

    Separate validate logic from read/write machinery
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- wasm/modules/utxotx/src/lib.rs
+++ wasm/modules/utxotx/src/lib.rs
@@ -10,6 +10,66 @@ use protobuf::{parse_from_bytes, Message};
 use signature::Verifier;
 use simple_asn1::{oid, ASN1Block, ASN1DecodeErr, BigUint, OID};
 
+#[no_mangle]
+pub extern "C" fn validate(stream: i32, input_len: i32) -> i32 {
+    let req_bytes: &mut Vec<u8> = &mut Vec::with_capacity(input_len as usize);
+    if batik::read(stream, req_bytes) != input_len as isize {
+        return -1;
+    }
+
+    match validate_tx(req_bytes) {
+        Ok(res) if batik::write(stream, &res) == res.len() as isize => 0,
+        Err(_) => -1,
+        _ => -1,
+    }
+}
+
+fn validate_tx(req_bytes: &mut Vec<u8>) -> Result<Vec<u8>, ASN1DecodeErr> {
+    // Decode the bytes into the ResolvedTransaction protobuf
+    let request = parse_from_bytes::<validation_api::ValidateRequest>(&req_bytes).unwrap();
+    let resolved_tx = request.get_resolved_transaction();
+    let txid = resolved_tx.get_txid();
+    batik::log(format!("txid {}", hex::encode(txid)).as_str());
+
+    let signatures = resolved_tx.get_signatures().to_vec();
+    for signer in required_signers(resolved_tx) {
+        let sig = signature(&signatures, signer.get_public_key()).unwrap();
+        batik::log(format!("sig {}", hex::encode(sig.get_signature())).as_str());
+
+        let pkix = sig.get_public_key();
+        batik::log(format!("pkix {}", hex::encode(pkix)).as_str());
+        let sec1 = extract_sec1_key(pkix).unwrap();
+        batik::log(format!("pk {}", hex::encode(&sec1)).as_str());
+        let vk = VerifyingKey::from_sec1_bytes(&sec1).unwrap();
+        batik::log(format!("vk {}", hex::encode(vk.to_encoded_point(false).to_bytes()),).as_str());
+        let signature = Signature::from_asn1(sig.get_signature()).unwrap();
+
+        if !vk.verify(txid, &signature).is_ok() {
+            batik::log(format!("signature is not valid!").as_str());
+            return Err(ASN1DecodeErr::Incomplete);
+        }
+    }
+
+    validation_api::ValidateResponse::new()
+        .write_to_bytes()
+        .map_err(|_| ASN1DecodeErr::Incomplete)
+}
+
+fn required_signers(resolved: &resolved::ResolvedTransaction) -> Vec<transaction::Party> {
+    let mut required = resolved.get_required_signers().to_vec();
+    for input in resolved.get_inputs() {
+        required.append(&mut input.get_state().get_info().get_owners().to_vec());
+    }
+    required
+}
+
+fn signature<'a>(
+    signatures: &'a Vec<transaction::Signature>,
+    public_key: &[u8],
+) -> Option<&'a transaction::Signature> {
+    signatures.iter().find(|sig| sig.public_key == public_key)
+}
+
 fn extract_sec1_key(pkix_subject_key: &[u8]) -> Result<Vec<u8>, ASN1DecodeErr> {
     let der = simple_asn1::from_der(pkix_subject_key)?; // map_err ?
     let block = der.first().ok_or_else(|| ASN1DecodeErr::Incomplete)?;
@@ -41,61 +101,4 @@ fn extract_sec1_key(pkix_subject_key: &[u8]) -> Result<Vec<u8>, ASN1DecodeErr> {
     };
 
     Ok(pk.to_vec())
-}
-
-#[no_mangle]
-pub extern "C" fn validate(stream: i32, input_len: i32) -> i32 {
-    let req_bytes: &mut Vec<u8> = &mut Vec::with_capacity(input_len as usize);
-    let read_len = batik::read(stream, req_bytes);
-    if read_len != input_len as isize {
-        return -1;
-    }
-
-    // Decode the bytes into the ResolvedTransaction protobuf
-    let request = parse_from_bytes::<validation_api::ValidateRequest>(&req_bytes).unwrap();
-    let resolved_tx = request.get_resolved_transaction();
-    let txid = resolved_tx.get_txid();
-    batik::log(format!("txid {}", hex::encode(txid)).as_str());
-
-    for signer in required_signers(resolved_tx) {
-        let signatures = resolved_tx.get_signatures().to_vec();
-        let sig = signature(&signatures, signer.get_public_key()).unwrap();
-        batik::log(format!("sig {}", hex::encode(sig.get_signature())).as_str());
-
-        let pkix = sig.get_public_key();
-        batik::log(format!("pkix {}", hex::encode(pkix)).as_str());
-        let sec1 = extract_sec1_key(pkix).unwrap();
-        batik::log(format!("pk {}", hex::encode(&sec1)).as_str());
-        let vk = VerifyingKey::from_sec1_bytes(&sec1).unwrap();
-        batik::log(format!("vk {}", hex::encode(vk.to_encoded_point(false).to_bytes()),).as_str());
-        let signature = Signature::from_asn1(sig.get_signature()).unwrap();
-
-        if !vk.verify(txid, &signature).is_ok() {
-            batik::log(format!("signature is not valid!").as_str());
-            return -1;
-        }
-    }
-
-    let result = validation_api::ValidateResponse::new();
-    let encoded_result = result.write_to_bytes().unwrap();
-    if batik::write(stream, &encoded_result) != encoded_result.len() as isize {
-        return -1;
-    }
-    0
-}
-
-fn required_signers(resolved: &resolved::ResolvedTransaction) -> Vec<transaction::Party> {
-    let mut required = Vec::new();
-    for input in resolved.get_inputs() {
-        required.append(&mut input.get_state().get_info().get_owners().to_vec());
-    }
-    required.append(&mut resolved.get_required_signers().to_vec());
-    required
-}
-
-fn signature<'a>(
-    signatures: &'a Vec<transaction::Signature>,
-    public_key: &[u8],
-) -> Option<&'a transaction::Signature> {
-    signatures.iter().find(|sig| sig.public_key == public_key)
 }
