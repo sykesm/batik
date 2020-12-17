a79e04c7875590a6cebbdbf13f78797f4db57b73
Matthew Sykes <sykesmat@us.ibm.com>
2020-12-16 22:39:51 -0500

    Additional tests for WASM signature validator
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- wasm/modules/utxotx/src/lib.rs
+++ wasm/modules/utxotx/src/lib.rs
@@ -83,7 +83,7 @@ pub extern "C" fn validate(stream: i32, input_len: i32) -> i32 {
     }
 }
 
-fn validate_tx(req_bytes: &mut Vec<u8>) -> Result<Vec<u8>> {
+fn validate_tx(req_bytes: &Vec<u8>) -> Result<Vec<u8>> {
     let request: ValidateRequest = parse_from_bytes(req_bytes)?;
     let tx = request.get_resolved_transaction();
 
@@ -169,6 +169,10 @@ fn extract_sec1_key(pkix_subject_key: &[u8]) -> Result<Vec<u8>> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use messages::resolved::ResolvedState;
+    use messages::transaction::{State, StateInfo};
+    use p256::pkcs8::FromPrivateKey;
+    use signature::Signer;
     use simple_asn1::ASN1Block;
 
     macro_rules! assert_error_match {
@@ -178,6 +182,137 @@ mod tests {
                 e => assert!(false, "expected {:?}, got {:?}", stringify!($error), e),
             }
         };
+    }
+
+    // DER encoded PKCS#8 private key for P256 was generated out of band to
+    // avoid transitive wasi/wasm dependencies from the random number crates
+    fn signing_key() -> ecdsa::SigningKey {
+        let key = "308187020100301306072a8648ce3d020106082a8648ce3d030107046d306b02\
+                   01010420ad86294ee2ae7f03461a4fbf0b65c11ac543cb5a185b0613f343c180\
+                   6d190979a14403420004a4c41859f75791ec327153b58afa5575b9dfab4ca441\
+                   62557f7a8d54acd2bbfe1c95f9c765182e01e28c6df04ca1399f68b0fb844391\
+                   d5d61c17ddf8162a8aa0";
+        let der = hex::decode(key).unwrap();
+        let sk = p256::SecretKey::from_pkcs8_der(&der).unwrap();
+        sk.into()
+    }
+
+    fn pkix_from_sec1(key: &[u8]) -> Vec<u8> {
+        let mut algid: Vec<ASN1Block> = Vec::new();
+        algid.push(ASN1Block::ObjectIdentifier(0, ec_public_key_oid()));
+        algid.push(ASN1Block::ObjectIdentifier(0, ec_p256v1_oid()));
+
+        let mut seq: Vec<ASN1Block> = Vec::new();
+        seq.push(ASN1Block::Sequence(0, algid));
+        seq.push(ASN1Block::BitString(0, 1, key.to_vec()));
+
+        let block = ASN1Block::Sequence(0, seq);
+        simple_asn1::to_der(&block).unwrap()
+    }
+
+    #[test]
+    fn validate_tx_parse_failure() {
+        assert_error_match!(validate_tx(&vec![1u8, 2, 3]), Error::ProtobufError(_));
+    }
+
+    #[test]
+    fn validate_tx_no_required_sigs() {
+        let req = ValidateRequest::new().write_to_bytes().unwrap();
+        let res = validate_tx(&req).unwrap();
+        let resp: ValidateResponse = parse_from_bytes(&res).unwrap();
+        assert_eq!(resp.get_valid(), true);
+    }
+
+    #[test]
+    fn validate_tx_signature_without_signer() {
+        let mut sig = Signature::new();
+        sig.set_public_key(vec![1, 2, 3]);
+        let mut resolved = ResolvedTransaction::new();
+        resolved.signatures.push(sig);
+        let mut req = ValidateRequest::new();
+        req.set_resolved_transaction(resolved);
+
+        let res = validate_tx(&req.write_to_bytes().unwrap()).unwrap();
+        let resp: ValidateResponse = parse_from_bytes(&res).unwrap();
+        assert!(resp.get_valid() == true);
+    }
+
+    #[test]
+    fn validate_tx_required_signer_no_sig() {
+        let mut party = Party::new();
+        party.public_key = vec![1u8, 2, 3];
+        let mut resolved = ResolvedTransaction::new();
+        resolved.required_signers.push(party);
+        let mut req = ValidateRequest::new();
+        req.set_resolved_transaction(resolved);
+
+        let res = validate_tx(&req.write_to_bytes().unwrap()).unwrap();
+        let resp: ValidateResponse = parse_from_bytes(&res).unwrap();
+        assert!(resp.get_valid() == false);
+    }
+
+    #[test]
+    fn validate_tx_matching_signature() {
+        let sk = signing_key();
+        let pk = sk.verify_key().to_encoded_point(false);
+        let pkix = pkix_from_sec1(pk.as_bytes());
+        let txid = "transaction-id";
+
+        let mut party = Party::new();
+        party.public_key = pkix.to_vec();
+
+        let mut sig = Signature::new();
+        sig.public_key = pkix.to_vec();
+        sig.signature = sk.sign(txid.as_bytes()).to_asn1().as_bytes().to_vec();
+
+        let mut resolved = ResolvedTransaction::new();
+        resolved.txid = txid.as_bytes().to_vec();
+        resolved.required_signers.push(party);
+        resolved.signatures.push(sig);
+
+        let mut req = ValidateRequest::new();
+        req.set_resolved_transaction(resolved);
+
+        let res = validate_tx(&req.write_to_bytes().unwrap()).unwrap();
+        let resp: ValidateResponse = parse_from_bytes(&res).unwrap();
+        assert!(resp.get_valid());
+    }
+
+    #[test]
+    fn tx_required_signers() {
+        let mut resolved = ResolvedTransaction::new();
+        for i in 1..=3 {
+            let mut party = Party::new();
+            party.public_key = format!("required{}", i).as_bytes().to_vec();
+            resolved.required_signers.push(party);
+        }
+        for i in 1..=3 {
+            let mut party = Party::new();
+            party.public_key = format!("owner{}", i).as_bytes().to_vec();
+            let mut info = StateInfo::new();
+            info.owners.push(party);
+            let mut state = State::new();
+            state.set_info(info);
+            let mut rs = ResolvedState::new();
+            rs.set_state(state);
+            resolved.inputs.push(rs);
+        }
+
+        let required = required_signers(&resolved);
+        let result: Vec<&str> = required
+            .iter()
+            .map(|p| p.get_public_key())
+            .map(|v| std::str::from_utf8(v).unwrap())
+            .collect();
+        let expected: Vec<&str> = vec![
+            "required1",
+            "required2",
+            "required3",
+            "owner1",
+            "owner2",
+            "owner3",
+        ];
+        assert_eq!(result, expected);
     }
 
     #[test]
@@ -295,7 +430,7 @@ mod tests {
     #[test]
     fn sec1_key_extraction_happy() {
         let mut algid: Vec<ASN1Block> = Vec::new();
-        algid.push(ASN1Block::ObjectIdentifier(0, oid!(1, 2, 840, 10045, 2, 1)));
+        algid.push(ASN1Block::ObjectIdentifier(0, ec_public_key_oid()));
         algid.push(ASN1Block::ObjectIdentifier(0, ec_p256v1_oid()));
 
         let mut seq: Vec<ASN1Block> = Vec::new();
