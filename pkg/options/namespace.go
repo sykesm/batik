be0f7dea79e5e559946914beefe2816e321eaec8
Jason Yellick <jyellick@us.ibm.com>
2021-02-03 13:14:34 -0500

    Add some accidentally skipped untracked files
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>

---
+++ pkg/options/namespace.go
@@ -0,0 +1,58 @@
+// Copyright IBM Corp. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package options
+
+import (
+	cli "github.com/urfave/cli/v2"
+)
+
+// Namespace exposes configuration for a namespace.
+type Namespace struct {
+	// Name is the human readable name for this namespace and how external
+	// transactors reference
+	Name string `yaml:"name"`
+
+	// DataDir is the path to the directory where ledger artifacts will be
+	// placed. If a relative path is used, the path is relative to the working
+	// directory unless the path was retrieved from a configuration file, in
+	// which case it is relative to the directory containing the configuration
+	// file.  Note, the namespace name is always concatenated with the DataDir
+	// to avoid collisions, so multiple namespaces may safely specify the same
+	// DataDir.
+	DataDir string `yaml:"data_dir,omitempty" batik:"relpath"`
+}
+
+// NamespaceDefaults returns the default configuration values for the ledger.
+func NamespaceDefaults() *Namespace {
+	return &Namespace{
+		DataDir: "data",
+	}
+}
+
+// ApplyDefaults applies default values for missing configuration fields.
+func (n *Namespace) ApplyDefaults() {
+	defaults := NamespaceDefaults()
+	if n.DataDir == "" {
+		n.DataDir = defaults.DataDir
+	}
+}
+
+// Flags exposes configuration fields as flags. The current value of the
+// receiver is used as the default value of the flag so a ApplyDefaults should
+// be called before requesting flags.
+func (n *Namespace) Flags() []cli.Flag {
+	def := NamespaceDefaults()
+	return []cli.Flag{
+		NewStringFlag(&cli.StringFlag{
+			Name:        "data-dir",
+			Value:       n.DataDir,
+			Destination: &n.DataDir,
+			Usage: flow(`Sets the data directory for all namespaces in the configuration.  Because
+					the data directory is concatenated with the namespace name when creating
+					artifacts inside the data directory, this value is safe to set with multiple
+					namespaces without fear of collision.`),
+			DefaultText: def.DataDir,
+		}),
+	}
+}
---
+++ pkg/options/namespace_test.go
@@ -0,0 +1,93 @@
+// Copyright IBM Corp. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package options
+
+import (
+	"flag"
+	"testing"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestNamespaceDefaults(t *testing.T) {
+	gt := NewGomegaWithT(t)
+	ledger := NamespaceDefaults()
+	gt.Expect(ledger).To(Equal(&Namespace{
+		DataDir: "data",
+	}))
+}
+
+func TestNamespaceApplyDefaults(t *testing.T) {
+	tests := map[string]struct {
+		setup func(*Namespace)
+	}{
+		"empty":    {setup: func(l *Namespace) { *l = Namespace{} }},
+		"data dir": {setup: func(l *Namespace) { l.DataDir = "" }},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			gt := NewGomegaWithT(t)
+
+			input := NamespaceDefaults()
+			tt.setup(input)
+
+			input.ApplyDefaults()
+			gt.Expect(input).To(Equal(NamespaceDefaults()))
+		})
+	}
+}
+
+func TestNamespaceFlagNames(t *testing.T) {
+	gt := NewGomegaWithT(t)
+	flags := (&Namespace{}).Flags()
+
+	var names []string
+	for _, f := range flags {
+		names = append(names, f.Names()...)
+	}
+
+	gt.Expect(flags).To(HaveLen(1))
+	gt.Expect(names).To(ConsistOf(
+		"data-dir",
+	))
+}
+
+func TestNamespaceFlags(t *testing.T) {
+	tests := map[string]struct {
+		args     []string
+		expected Namespace
+	}{
+		"no flags": {
+			args:     []string{},
+			expected: Namespace{},
+		},
+		"data dir": {
+			args:     []string{"--data-dir=some/path/name"},
+			expected: Namespace{DataDir: "some/path/name"},
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			gt := NewGomegaWithT(t)
+
+			ledger := &Namespace{}
+			flagSet := flag.NewFlagSet("ledger-test", flag.ContinueOnError)
+			for _, f := range ledger.Flags() {
+				err := f.Apply(flagSet)
+				gt.Expect(err).NotTo(HaveOccurred())
+			}
+
+			err := flagSet.Parse(tt.args)
+			gt.Expect(err).NotTo(HaveOccurred())
+			gt.Expect(ledger).To(Equal(&tt.expected))
+		})
+	}
+}
+
+func TestNamespaceFlagsDefaultText(t *testing.T) {
+	flags := NamespaceDefaults().Flags()
+	assertWrappedFlagWithDefaultText(t, flags...)
+}
