2c8f68e3d55860b7badcd05a8e3bb5be1d68f463
Matthew Sykes <sykesmat@us.ibm.com>
2021-02-04 14:57:13 -0500

    Add gofumpt to version controlled tools
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- tools/go.mod
+++ tools/go.mod
@@ -29,4 +29,5 @@ require (
 	google.golang.org/grpc v1.33.0 // indirect
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.0.1
 	google.golang.org/protobuf v1.25.0
+	mvdan.cc/gofumpt v0.1.0
 )
--- tools/tools.go
+++ tools/tools.go
@@ -12,4 +12,5 @@ import (
 	_ "github.com/uber/prototool/cmd/prototool"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+	_ "mvdan.cc/gofumpt"
 )
