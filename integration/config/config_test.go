fb967a3892a73cf23ca19380839d4f66e9346aab
Matthew Sykes <sykesmat@us.ibm.com>
2021-02-05 19:41:15 -0500

    Add some tests to show flags override config files
    We have a problem where global flags do not override the elements read
    from the configuration file.
    
    The current process does something like this:
    
    1. Create a default configuration object
    2. Register flags that update the configuration object
    3. Start the cli.App with Run
    4. The cli.App creates a flag.FlagSet, parses the flags, and sets the
       flag values.
    5. The Before hook is called by Run and we get the value of the
       `--config` flag and then read the file.
    
    Once we get to 5, the values from the configuration file overwrite what
    was written when the flags were parsed.
    
    For command flags (not global flags), the command line processing occurs
    after the app's Before hook runs so they *do* overwrite the values read
    from the configuration file.
    
    And so, we have a bit of a chicken and egg problem. We want to take
    advantage of the flag support from the cli but we need to read the
    configuration file before the flags are processed.
    
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- integration/config/config_test.go
+++ integration/config/config_test.go
@@ -35,7 +35,17 @@ var _ = Describe("Command Line Configuration", func() {
 		}
 	})
 
-	Context("with empty configuration file", func() {
+	Context("without a configuration file", func() {
+		It("uses default values", func() {
+			cmd := exec.Command(batikPath, "--show-config")
+			cmd.Dir = tempdir
+
+			config := decodeShowConfigOutput(cmd)
+			Expect(config).To(Equal(options.BatikDefaults()))
+		})
+	})
+
+	Context("with an empty configuration file", func() {
 		BeforeEach(func() {
 			err := ioutil.WriteFile(filepath.Join(tempdir, "batik.yaml"), []byte("---\n"), 0o644)
 			Expect(err).NotTo(HaveOccurred())
@@ -151,6 +161,74 @@ var _ = Describe("Command Line Configuration", func() {
 			Expect(config).To(Equal(expected))
 		})
 	})
+
+	Context("with a sparse configuration file", func() {
+		It("honors data_dir in the configuration file", func() {
+			config := &options.Batik{
+				DataDir: "/absolute/path",
+			}
+			writeConfigurationFile(filepath.Join(tempdir, "batik.yaml"), config)
+
+			cmd := exec.Command(batikPath, "--show-config")
+			cmd.Dir = tempdir
+
+			config.ApplyDefaults()
+			config.Server.TLS.CertsDir = filepath.Join(tempdir, "tls-certs")
+
+			active := decodeShowConfigOutput(cmd)
+			Expect(active).To(Equal(config))
+		})
+
+		// This tests demonstrate that global flags do not override values
+		// from a configuration file.
+		PIt("global flags are prioritized over the configuration file", func() {
+			config := &options.Batik{
+				DataDir: "/absolute/path",
+			}
+			writeConfigurationFile(filepath.Join(tempdir, "batik.yaml"), config)
+
+			cmd := exec.Command(
+				batikPath,
+				"--show-config",
+				"--data-dir", "another/data/dir",
+			)
+			cmd.Dir = tempdir
+
+			config.ApplyDefaults()
+			config.DataDir = filepath.Join(tempdir, "another/data/dir")
+			config.Server.TLS.CertsDir = filepath.Join(tempdir, "tls-certs")
+
+			active := decodeShowConfigOutput(cmd)
+			Expect(active).To(Equal(config))
+		})
+
+		It("command flags are prioritized over the configuration file", func() {
+			config := &options.Batik{
+				Server: options.Server{
+					HTTP: options.HTTPServer{
+						ListenAddress: "http:http",
+					},
+				},
+			}
+			writeConfigurationFile(filepath.Join(tempdir, "batik.yaml"), config)
+
+			cmd := exec.Command(
+				batikPath,
+				"--show-config",
+				"start",
+				"--http-listen-address", "https:https",
+			)
+			cmd.Dir = tempdir
+
+			config.ApplyDefaults()
+			config.DataDir = filepath.Join(tempdir, "data")
+			config.Server.HTTP.ListenAddress = "https:https"
+			config.Server.TLS.CertsDir = filepath.Join(tempdir, "tls-certs")
+
+			active := decodeShowConfigOutput(cmd)
+			Expect(active).To(Equal(config))
+		})
+	})
 })
 
 func decodeShowConfigOutput(cmd *exec.Cmd) *options.Batik {
@@ -162,6 +240,15 @@ func decodeShowConfigOutput(cmd *exec.Cmd) *options.Batik {
 	err = yaml.NewDecoder(bytes.NewBuffer(sess.Out.Contents())).Decode(&config)
 	Expect(err).NotTo(HaveOccurred())
 	return &config
+}
+
+func writeConfigurationFile(path string, config *options.Batik) {
+	f, err := os.Create(path)
+	Expect(err).NotTo(HaveOccurred())
+	defer f.Close()
+
+	err = yaml.NewEncoder(f).Encode(config)
+	Expect(err).NotTo(HaveOccurred())
 }
 
 // macOS makes /tmp a symlink to /private/tmp
